3a7574f49d95dddbb79ff7690a04a9992330f232
Mangal Hang Limbu <lazehang@gmail.com>
2022-06-16 15:59:12 +0800

    test greet people

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,9 @@
-test("print list of names prefixed with Hello", function() {
+const greetPeople = require("./greet-people");
+test("print list of names prefixed with Hello", function () {
   // Arrange
+  const names = ["John", "Jake"];
   // Act
+  const greet = greetPeople(names);
   // Assert
+  expect(greet).toEqual("Hello JohnJake");
 });
